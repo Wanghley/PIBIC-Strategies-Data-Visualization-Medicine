ce4b85340705837c53a5d075b14eb5bebbb09035
wanghley <wanghleys@gmail.com>
2020-09-03 16:50:03 -0300

    [UPDATE]Threading link

--- docs/Threading.py
+++ docs/Threading.py
@@ -1,4 +1,5 @@
-from PyQt5.QtGui import *
+
+#https://www.learnpyqt.com/courses/concurrent-execution/multithreading-pyqt-applications-qthreadpool/from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 

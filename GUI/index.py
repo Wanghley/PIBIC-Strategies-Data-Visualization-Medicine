7c15a2fdaaa622ceaab3a88fdfcf18f8e67468e1
wanghley <wanghleys@gmail.com>
2020-09-10 20:17:11 -0300

    [UPDATE] screen update

--- GUI/index.py
+++ GUI/index.py
@@ -1,5 +1,5 @@
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
-import sys
+import sys, time, traceback
 import blue as ble
 import scripts as scpt
 import patient
@@ -8,6 +8,76 @@ device = None
 folder = None
 getData = False
 f = None
+
+
+
+class WorkerSignals(QtCore.QObject):
+    '''
+    Defines the signals available from a running worker thread.
+    Supported signals are:
+
+                    finished
+                        No data
+                    
+                    error
+                        `tuple` (exctype, value, traceback.format_exc() )
+                    
+                    result
+                        `object` data returned from processing, anything
+
+                    progress
+                        `int` indicating % progress 
+
+    '''
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+    progress = pyqtSignal(int)
+
+
+class Worker(QtCore.QRunnable):
+    '''
+    Worker thread
+
+    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
+            :param callback: The function callback to run on this worker thread. Supplied args and 
+                            kwargs will be passed through to the runner.
+            :type callback: function
+            :param args: Arguments to pass to the callback function
+            :param kwargs: Keywords to pass to the callback function
+
+    '''
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()    
+
+        # Add the callback to our kwargs
+        self.kwargs['progress_callback'] = self.signals.progress        
+
+    @pyqtSlot()
+    def run(self):
+        '''
+        Initialise the runner function with passed args, kwargs.
+        '''
+        
+        # Retrieve args/kwargs here; and fire processing using them
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)  # Return the result of the processing
+        finally:
+            self.signals.finished.emit()  # Done
+
 
 #/////////////////////////////////////////////////////////////////////////////////////////////////
 # Bluetooth Connection Dialog

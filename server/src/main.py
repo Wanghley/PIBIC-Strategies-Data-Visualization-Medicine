2965c280697d9311d55addc4256f4280a74efb48
wanghley <wanghleys@gmail.com>
2020-11-26 18:25:33 -0300

    [UPDATE] main controller code is now operational

--- server/src/main.py
+++ server/src/main.py
@@ -1,8 +1,10 @@
 from time import sleep
 import threads as th
 import data_structure as ds
-import bluetooth, patient
+import bluetooth
+import patient as p
 from datetime import datetime
+import sys
 
 if __name__ == "__main__":
     file_path=''
@@ -13,16 +15,18 @@ if __name__ == "__main__":
     print('birthday:')
     birthday=input() # MM/DD/YYYY
 
-    p = patient.Patient(name,sex,birthday)
+    patient = p.Patient(name,sex,birthday)
 
     try:
         time = datetime.now().strftime("%d%m%Y-%H-%M-%S")
         print(time)
-        file_path = p.name+"_"+time+".csv"
+        file_path = patient.name+"_"+time+".csv"
         print(file_path)
         open(file_path, 'w+').close()
-    except:
+    except Exception as e:
         print("ERROR CREATING CSV DATA FILE!!")
+        print(e)
+        sys.exit(1)
 
     print ("\n"+"-"*50+"\nStarting in 1 seconds...")
     sleep(1)
@@ -54,7 +58,7 @@ if __name__ == "__main__":
 
     # imp.reload(th)
     st = th.BluetoothAcquisitionThread(data_buffer, sock, True)
-    pt = th.DataSavingThread(data_buffer, p, file_path, True)
+    pt = th.DataSavingThread(data_buffer, patient, file_path, True)
 
     st.start()
     pt.start()

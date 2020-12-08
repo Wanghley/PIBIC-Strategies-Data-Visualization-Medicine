497388cd6cfeac169d32dd95429a459bdc05f519
Wanghley <wanghleys@gmail.com>
2020-12-08 14:54:57 -0300

    [UPDATE] code refactoring

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,4 @@
-# This Python file uses the following encoding: utf-8
+# This Python file uses the encoding: utf-8
 import sys
 import os
 
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
-    print("hello world")
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec_())

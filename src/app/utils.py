35516cf2a71d9ff9a970a86d4040d1162231a41c
wanghley <wanghleys@gmail.com>
2021-02-03 23:59:04 -0300

    [UPDATE] header save function

--- src/app/utils.py
+++ src/app/utils.py
@@ -2,8 +2,10 @@ from patient import Patient
 from datetime import datetime
 import bluetooth
 import time
+import csv
 
-def createFile(path,patient,file_type):
+
+def createFile(path, patient, file_type):
     try:
         time = datetime.now().strftime("%d%m%Y-%H-%M-%S")
         file_path = path+"/"+patient.name+"_"+time+".csv"
@@ -13,19 +15,32 @@ def createFile(path,patient,file_type):
         print("ERROR CREATING CSV DATA FILE!!")
         print(e)
 
-#TODO
-def createHeader(path,patient):
-    return 0
+def createHeader(file_path, patient, tsk_duration, tasks, startTime, header):
+    try:
+        with open(file_path, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([startTime, "task duration: " +
+                            tsk_duration, "tasks: "+str(tasks)])
+            writer.writerow(['name:'+patient.name, 'sex:'+patient.sex,
+                            'birthday:'+patient.birthday])
+            writer.writerow([header])
+            csvfile.close()
+    except Exception as e:
+        print(e)
+        print("Error creating header!")
 
-def connectBluetooth(address,port=1):
+
+def connectBluetooth(address, port=1):
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((address, port))
     time.sleep(2)
 
     return sock
 
+
 def findBluetoothDevices(duration):
-    nearby_devices = bluetooth.discover_devices(duration=duration,lookup_names=True)
+    nearby_devices = bluetooth.discover_devices(
+        duration=duration, lookup_names=True)
 
     devices = ['Select a device']
 

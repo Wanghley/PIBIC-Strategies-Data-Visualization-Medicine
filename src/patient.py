c37f293087e4574ee8ded10db79a48f8cf0d29c3
wanghley <wanghleys@gmail.com>
2021-02-18 21:04:58 -0300

    [UPDATE] refactoring
    removing not used files or old files

--- src/patient.py
+++
@@ -1,6 +0,0 @@
-class Patient:
-    def __init__(self, name, birthday, sex, parkinsonian):
-        self.name = name
-        self.birthday = birthday
-        self.sex = sex
-        self.parkinsonian = parkinsonian

657f5166a9709808f68856e60bb560c191168375
Wanghley Soares Martins <wanghleys@gmail.com>
2021-09-09 22:50:39 -0300

    [UPDATE] top bar, page stack area, and botton bar added

--- src/app/design/windows/main_window/ui_main_window.py
+++ src/app/design/windows/main_window/ui_main_window.py
@@ -23,6 +23,9 @@ class UI_MainWindow(object):
         parent.resize(960,540) # initial size
         parent.setMinimumSize(650,400) #minimun size
 
+        #-------------------------------------------------------------------------------------------------
+        # MAIN WIDGET AND CHILDREN
+        #-------------------------------------------------------------------------------------------------
         #create central widget
         self.central_frame = QFrame()
         self.central_frame.setStyleSheet("background-color: #282a36")
@@ -32,16 +35,45 @@ class UI_MainWindow(object):
         self.main_layout.setContentsMargins(0,0,0,0) # remove borders around left menu and content layout
         self.main_layout.setSpacing(0) # remove borders between left menu and content layout
 
-        # Lef menu
+        # Left menu
         self.left_menu = QFrame()
         self.left_menu.setStyleSheet("background-color: #44475a")
         self.left_menu.setMaximumWidth(50) #change maximum width of left menu to 50px
+        self.left_menu.setMinimumWidth(50)
 
+        #-------------------------------------------------------------------------------------------------
+        # CONTENT WIDGET & CHILDREN
+        #-------------------------------------------------------------------------------------------------
         # content
         self.content = QFrame()
         self.content.setStyleSheet("background-color: #282a36")
 
+        # content layout
+        self.content_layout = QVBoxLayout(self.content)
+        self.content_layout.setContentsMargins(0,0,0,0) 
+        self.content_layout.setSpacing(0)
 
+        # top bar
+        self.top_bar = QFrame()
+        self.top_bar.setMinimumHeight(30) # Always set minimum anx maximum height to stay responsible
+        self.top_bar.setMaximumHeight(30)
+        self.top_bar.setStyleSheet("background-color:#21232d; color:#6272a4")
+
+        # Application pages
+        self.pages = QStackedWidget() # used to create page inside of the application
+        self.pages.setStyleSheet("background-color:; font-size: 12pt; color: #f8f8f2")
+
+        # botton bar
+        self.botton_bar = QFrame()
+        self.botton_bar.setMinimumHeight(30) # Always set minimum anx maximum height to stay responsible
+        self.botton_bar.setMaximumHeight(30)
+        self.botton_bar.setStyleSheet("background-color:#21232d; color:#6272a4")
+
+        # add widgets to layout
+        self.content_layout.addWidget(self.top_bar)
+        self.content_layout.addWidget(self.pages)
+        self.content_layout.addWidget(self.botton_bar)
+        #-------------------------------------------------------------------------------------------------
         # add widgets to app
         self.main_layout.addWidget(self.left_menu)
         self.main_layout.addWidget(self.content)

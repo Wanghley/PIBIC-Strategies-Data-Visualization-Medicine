23fc089ce4be86fbccfb0f13b4b6d7025d823938
Wanghley Soares Martins <wanghleys@gmail.com>
2021-10-22 14:01:09 -0300

    [UPDATE] icon drawing function bug fixing

--- src/app/widgets/py_push_button.py
+++ src/app/widgets/py_push_button.py
@@ -13,8 +13,9 @@
 import os
 from qt_core import *
 
+
 class PyPushButton(QPushButton):
-    def __init__(self,text="",height=40,minimun_width=50,text_padding=55,text_color="#c3ccdf",icon_path="",icon_color="#c3ccdf",btn_color="#44475a",btn_hover="#4f5368",btn_pressed="#282a36",is_active=False):
+    def __init__(self, text="", height=40, minimun_width=50, text_padding=55, text_color="#c3ccdf", icon_path="", icon_color="#c3ccdf", btn_color="#44475a", btn_hover="#4f5368", btn_pressed="#282a36", is_active=False):
         super().__init__()
 
         # set default parameters
@@ -34,11 +35,10 @@ class PyPushButton(QPushButton):
         self.btn_pressed = btn_pressed
         self.is_active = is_active
 
-        self.setStyle(text_padding=self.text_padding, text_color=self.text_color,btn_color=self.btn_color, btn_hover = self.btn_hover, btn_pressed=self.btn_pressed, is_active=self.is_active)
+        self.setStyle(text_padding=self.text_padding, text_color=self.text_color, btn_color=self.btn_color,
+                      btn_hover=self.btn_hover, btn_pressed=self.btn_pressed, is_active=self.is_active)
 
-    
-
-    def setStyle(self,text_padding=55,text_color="#c3ccdf",btn_color="#44475a",btn_hover="#4f5368",btn_pressed="#282a36",is_active=False):
+    def setStyle(self, text_padding=55, text_color="#c3ccdf", btn_color="#44475a", btn_hover="#4f5368", btn_pressed="#282a36", is_active=False):
         style = f"""
             QPushButton {{
                 color:{text_color};
@@ -65,37 +65,39 @@ class PyPushButton(QPushButton):
             self.setStyleSheet(style)
         else:
             self.setStyleSheet(style+active_style)
-    
-    def paintEvent(self, event):
-        #return default style
-        QPushButton.paintEvent(self,event)
 
-        #painter
+    def paintEvent(self, event):
+        # return default style
+        QPushButton.paintEvent(self, event)
+
+        # painter
         qp = QPainter()
         qp.begin(self)
         qp.setRenderHint(QPainter.Antialiasing)
-        qp.setPen(Qt.NoPen)#create a border, but we don't want borders
+        qp.setPen(Qt.NoPen)  # create a border, but we don't want borders
 
-        rect = QRect(0,0,self.minimum_width,self.height())
+        rect = QRect(0, 0, self.minimum_width, self.height())
+
+        self.draw_icon(qp, self.icon_path, rect, self.icon_color)
 
         qp.end()
 
-    
     def draw_icon(self, qp, image, rect, color):
         # get folder of icons for any system
-        app_path = os.path.abspath(os.getcwd) #root of application
-        folder_path = "design/icons"
-        path = os.path.join(app_path,folder_path)
-        icon_path = os.path.normpath(os.path.join(path,image))
-
-        #Draw icon
-        icon = QPixmap(icon_path)
-        painter = QPainter(icon)
-        painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
-        painter.fillRect(icon.rect(), color)
-        qp.drawPixMap(
-            (rect.width()-icon.width())/2,
-            (rect.height()-icon.height())/2,
-            icon
-        )
-        painter.end()
+        if(len(image) > 1):
+            app_path = os.path.abspath(os.getcwd())  # root of application
+            folder_path = "src/app/design/icons"
+            path = os.path.join(app_path, folder_path)
+            icon_path = os.path.normpath(os.path.join(path, image))
+            
+            # Draw icon
+            icon = QPixmap(icon_path)
+            painter = QPainter(icon)
+            painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
+            painter.fillRect(icon.rect(), color)
+            qp.drawPixmap(
+                (rect.width()-icon.width())/2,
+                (rect.height()-icon.height())/2,
+                icon
+            )
+            painter.end()
